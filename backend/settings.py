5368051c9b27bcaba5edf216f4270bd87a130766
lagsaa <mingo.angelyn.olimba@gmail.com>
2025-04-16 18:32:45 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -87,10 +87,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-import dj_database_url
+import os
 
 DATABASES = {
-    'default': dj_database_url.config(default="sqlite:///db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
